5f10f7fff0bc318574aadf3f298212039a7d4d6d
Tamara <Tamara@LAPTOP-KFHEUE2T>
2021-03-11 17:58:35 +0100

    .

--- TareapresencialTamaraRosa/src/tareapresencial2ev/CaminoBasico.java
+++ TareapresencialTamaraRosa/src/tareapresencial2ev/CaminoBasico.java
@@ -1,10 +1,10 @@
 package tareapresencial2ev;
 public class CaminoBasico {
-	public int miMetodo(int a, int b) {
+	public int miMetodo(int numero1, int numero2) {
 		int resultado = 0;
-		if (a > 5) {
+		if (numero1 > 5) {
 			resultado = 55;
-		} else if (b < 10) {
+		} else if (numero2 < 10) {
 			resultado = 100;
 		}
 		return resultado;
