71d83ad87ac3c021cd02b4426df16e88354b4d67
Tamara <Tamara@LAPTOP-KFHEUE2T>
2021-03-11 17:30:28 +0100

    primera versión

---
+++ TareapresencialTamaraRosa/src/tareapresencial2ev/CaminoBasico.java
@@ -0,0 +1,12 @@
+package tareapresencial2ev;
+public class CaminoBasico {
+	public int miMetodo(int a, int b) {
+		int resultado = 0;
+		if (a > 5) {
+			resultado = 55;
+		} else if (b < 10) {
+			resultado = 100;
+		}
+		return resultado;
+	}
+}
